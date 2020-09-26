78f1ec403b35ee368bfd1f71ee37032e14fdbcfe
misuzz <fs9dm3cc8ct1@gmail.com>
2020-09-26 23:09:03 +0900

    change layout a lot

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Search from './Search';
+import Search from './Search';　//.jsのファイルだからこの書き方（✗ import './Search.js'）//
+import Animal from './Animal-img';
 
 function App() {
   const mainTitle = "Serching Animal Images"
@@ -9,21 +9,29 @@ function App() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={Animal} className= "Animal-logo" alt="木に群がる動物" />
           <h1>{mainTitle}</h1>
           <h2>{subTitle}</h2>
-          <Search />
-          <a
+        </header>
+
+        <body className= "App-body">
+          <div>
+          <Search/>
+          <h3>ここに画像を出す</h3>
+          </div>
+        </body>  
+
+        <footer className= "App-footer">
+        <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            なんかリンク貼る？
+             about <span class="two"> me </span>.
         </a>
-        </header>
-        <body className= "allimages">
-        </body>  
+        </footer>
+
       </div>
     );
   };
--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,51 @@
-import React from 'react'
+import React from 'react';
 import './Search.css';
+import Twitter from 'twitter-lite';
+
+const client = new Twitter({
+    subdomain: "cors-anywhere.herokuapp.com/https://api",
+    consumer_key: 'DZ16Dl9ONDgcztHlNMuUVIR6x',
+    consumer_secret: '9ifjSHk6LMvYfjf8GIbjAdLVR1aeHdQNlQi4GNjNGKtVjtNb4U',
+    bearer_token: 'AAAAAAAAAAAAAAAAAAAAAD%2BYHwEAAAAA%2BrLihuKzOcwzS%2BZVISAD%2F9dHJVM%3DM93E37OHrTGyP9XDlUASvEsFHPY1g238MP5SKFxdwG80Il6FY9'
+});
 
 class Search extends React.Component {
+
+    async requestTwitter() {
+        const result = await client.get('search/tweets', {
+            q: 'ニジマス',   //←？//
+            result_type: 'mixed',  //←？//
+            include_entities: 'true'   //←？//
+        });
+        var statuses = result.statuses     //←？//
+        statuses.forEach(element => {
+            if (element.entities.media !== null) {
+                var entitie = element.entities
+                if (entitie !== null) {
+                    if (entitie.media !== undefined) {
+                        entitie.media.forEach(element => {
+                            console.log(element.media_url)
+                        })
+                    }
+                } else {
+                    console.log("エラー")
+                }
+            } else {
+                console.log("エラー")
+            }
+        });
+        // statuses.forEach((status) => {
+        //     console.log(status.entities.media)
+        // })
+    }
+
     render() {
         return (
             <div className="searching">
-                <p>動物入れろ<span>→ </span></p>
-                <input　type= "text"/>
-                <button>検索</button>
+                <p>Animal's Name Here<span class="one">⇒</span></p>
+                <input/>
+                <button class="Button-style"
+                    type="button" onClick={() => this.requestTwitter()}> Search </button>
             </div>
         );
     }
